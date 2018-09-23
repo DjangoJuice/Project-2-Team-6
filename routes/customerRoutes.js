601d27be93ada9f12b52cc17d2e35dd23139de07
Sutras <37882933+Audiosutras@users.noreply.github.com>
2018-09-23 12:08:18 -0400

    Api routes completed. Will need to implement html routes

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -19,36 +19,43 @@ module.exports = function(app) {
   // Create Customer 
   app.post("/api/customers/", function(req, res) {
     db.Customer.create({
-      customerName: req.body.restaurantName,
-      customerPhone: req.body.restaurantAddress,
-      restaurantImg: req.body.restaurantImg,
-      restaurantDescription: req.body.TEXT,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }).then(function(dbRestaurant) {
-      res.json(dbRestaurant);
+        customerName: req.body.customerName,
+        customerPhone: req.body.customerPhone,
+        //customer email is passed from the user's email
+        customerEmail: req.body.email,
+        UserId: req.body.UserId
+    }).then(function(dbCustomer) {
+      res.json(dbCustomer);
     });
   });
 
-  // Update Restaurant
-  app.put("/api/restaurants/:id", function(req, res) {
-    db.Restaurant.update({
-      restaurantName: req.body.restaurantName,
-      restaurantAddress: req.body.restaurantAddress,
-      restaurantImg: req.body.restaurantImg,
-      restaurantDescription: req.body.TEXT,
-      updatedAt: new Date()
+   // Update Customer's email if the User's email is updated
+   app.put("/api/customers/:id", function(req, res) {
+    db.Customer.update({
+        customerEmail: req.body.customerEmail,
     },
     {
       where: {id: req.params.id}
     }).then(function() {
-      res.json({message: "Restaurant Updated"});
+      res.json({message: "Customer Updated"});
     });
   });
 
-  // Delete a Restaurant by supplying id
-  app.delete("/api/restaurants/:id", function(req, res) {
-    db.Restaurant.destroy({ where: { id: req.params.id } }).then(function() {
+  // Update a Customer's phone
+  app.put("/api/customers/:id", function(req, res) {
+    db.Customer.update({
+        customerPhone: req.body.customerPhone,
+    },
+    {
+      where: {id: req.params.id}
+    }).then(function() {
+      res.json({message: "Customer Updated"});
+    });
+  });
+
+  // Delete a Customer by supplying id
+  app.delete("/api/customers/:id", function(req, res) {
+    db.Customer.destroy({ where: { id: req.params.id } }).then(function() {
       res.json({message: "Restaurant Deleted"});
     });
   });
